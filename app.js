41be8c05c1eec93fea8ea734fc48bf5fa8b4185a
Benjamin Frost <43476782+BenMarkFrost@users.noreply.github.com>
2021-03-19 01:56:06 +0000

    Added more comprehensive referencing

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// Based off
-// https://acidtango.com/thelemoncrunch/how-to-implement-a-video-conference-with-webrtc-and-node/
+// The code below contains references to a tutorial where stated.
 
 const express = require('express')
 const app = express()
@@ -12,13 +11,39 @@ module.exports = app;
 
 let clientsDict = {};
 
+function sleep(minutes) {
+  ms = minutes * 60 * 1000;
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function hostTimer(socket, roomId){
+
+  // timeout for a single call is 5 minutes.
+  await sleep(5);
+  socket.emit("close");
+  clientsDict[roomId] = 0;
+
+}
+
+const port = process.env.PORT || 8080
+server.listen(port, () => {
+  console.log('Express server listening on port ' + port);
+});
+
+
+// From this point on the code has been taken from the tutorial hosted on acidtango.com. 
+// Excaptions to this where we have added our own code are explicitly stated. @ = created.
+// https://acidtango.com/thelemoncrunch/how-to-implement-a-video-conference-with-webrtc-and-node/
+
 io.on('connection', (socket) => {
   socket.on('join', (roomId) => {
-    let roomClients = clientsDict[roomId]
 
+    // @ Created to track the number of clients in each room.
+    let roomClients = clientsDict[roomId]
     if (!roomClients){
       roomClients = 0
     }
+    //
 
     console.log(roomClients)
 
@@ -27,7 +52,9 @@ io.on('connection', (socket) => {
       socket.join(roomId)
       socket.emit('room_created', roomId)
       clientsDict[roomId] = 1
+      // @ Added a timer so that the room resets after a predetermined amount of time.
       hostTimer(socket, roomId);
+      //
     } else if (roomClients == 1) {
       console.log('Joining room ' + roomId + ' and emitting room_joined socket event')
       socket.join(roomId)
@@ -56,27 +83,11 @@ io.on('connection', (socket) => {
     console.log('Broadcasting webrtc_ice_candidate event to peers in room ${event.roomId}')
     socket.broadcast.to(event.roomId).emit('webrtc_ice_candidate', event)
   })
+  // @ Added a socket acceptor for when second person leaves the room but the host stays.
   socket.on("peerDisconnected", (roomId) => {
     console.log("Peer left room " + roomId);
     clientsDict[roomId] = 1;
   })
+  //
 })
 
-function sleep(minutes) {
-  ms = minutes * 60 * 1000;
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function hostTimer(socket, roomId){
-
-  // timeout for a single call is 5 minutes.
-  await sleep(5);
-  socket.emit("close");
-  clientsDict[roomId] = 0;
-
-}
-
-const port = process.env.PORT || 8080
-server.listen(port, () => {
-  console.log('Express server listening on port ' + port);
-});
